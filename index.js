010095a5e07e25192592d5b2310d6a8453f7484c
sreeramsam <52190534+sreeramsam@users.noreply.github.com>
2021-01-22 17:49:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,113 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant - Menu</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-md-6 offset-md-2">
+            <img class="img-fluid" src="logo-1.png" style="height: 250px;">
+        </div>
+    </div>
+    <div class="container-fluid pt-4 pb-4">
+        <br>
+        <h2 class="text-center">~~ Menu ~~</h2>
+        <h4 class="text-center pb-3" style="color: darkcyan;">` ~~.~~ `</h4>
+        <div class="row">
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-5" id="c1">
+                <div class="ps">
+                    <div class="d-flex flex-row p-1 border-bottom" data-aos="fade-up">
+                        <div class="p-2 border-bottom">
+                            <img class="img rounded" src="dosa.jpg" style="height: 150px;">
+                        </div>
+                        <div class="pt-3 pl-3 pr-1 flex-grow-1 border-bottom">
+                            <h4 class="font-weight-bold">Dosa</h4>
+                                <h6>A Crispy and Delicious Dosa to Taste</h6>
+                                <h5 class="text-left">₹ 40</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row p-1 border-bottom" data-aos="fade-up">
+                        <div class="p-2 border-bottom">
+                            <img class="img rounded" src="idly.jpg" style="height: 136px;">
+                        </div>
+                        <div class="pt-3 pl-3 pr-1 flex-grow-1 border-bottom">
+                            <h4 class="font-weight-bold">Idly</h4>
+                                <h6>A Soft and Delicious Idly in your Plate</h6>
+                                <h5 class="text-left">₹ 20</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row p-1 border-bottom" data-aos="fade-up">
+                        <div class="p-2 border-bottom">
+                            <img class="img rounded" src="vada.jpg" style="height: 136px;">
+                        </div>
+                        <div class="pt-3 pl-3 pr-1 flex-grow-1 border-bottom">
+                            <h4 class="font-weight-bold">Vada</h4>
+                                <h6>A Soft and Delicious Vada in your Plate</h6>
+                                <h5 class="text-left">₹ 10</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5" id="c1">
+                <div class="ps">
+                    <div class="d-flex flex-row p-1 border-bottom" data-aos="fade-up">
+                        <div class="p-2 border-bottom">
+                            <img class="img rounded" src="chapathi.jpg" style="height: 150px;">
+                        </div>
+                        <div class="pt-3 pl-3 pr-1 flex-grow-1 border-bottom">
+                            <h4 class="font-weight-bold">Chapathi</h4>
+                                <h6>A Soft and Delicious Chapathi to Taste</h6>
+                                <h5 class="text-left">₹ 45</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row p-1 border-bottom" data-aos="fade-up">
+                        <div class="p-2 border-bottom">
+                            <img class="img rounded" src="poori.jpg" style="height: 136px;">
+                        </div>
+                        <div class="pt-3 pl-3 pr-1 flex-grow-1 border-bottom">
+                            <h4 class="font-weight-bold">Poori</h4>
+                                <h6>A Crispy and soft poori served with potato subji</h6>
+                                <h5 class="text-left">₹ 45</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row p-1 border-bottom" data-aos="fade-up">
+                        <div class="p-2 border-bottom">
+                            <img class="img rounded" src="pongal.jpg" style="height: 136px;width: 208">
+                        </div>
+                        <div class="pt-3 pl-3 pr-1 flex-grow-1 border-bottom">
+                            <h4 class="font-weight-bold">Pongal</h4>
+                                <h6>Delicious Pongal Served with Pure Ghee</h6>
+                                <h5 class="text-left">₹ 45</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1">
+            </div>
+        </div>
+    </div>
+    <div class=" form-group row col-md-7 offset-md-5">
+        <div class="col-md-6">
+            <a href="checkout.html"><button type="button" class="btn btn-danger text-nowrap">Check Out</button></a>
+        </div>      
+    </div>
+</body>
+<script>
+    AOS.init();
+</script>
+
+</html>
